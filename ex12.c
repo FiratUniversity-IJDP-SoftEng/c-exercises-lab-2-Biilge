13b1fc1946108134b793cc4b1f799e5f89ced33c
Biilge <biilgesena@gmail.com>
2025-12-07 22:42:54 +0300

    Add factorial calculation in ex12.c
    Implement factorial calculation for a given number.

--- ex12.c
+++ ex12.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int n;
+    int fact = 1;   // result is covered in fact
+    int i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // negative integer can not be factorial
+    if (n < 0)
+    {
+        printf("Negative numbers do not have factorial.\n");
+        return 0;
+    }
+
+    // factoriation 
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("%d! = %d\n", n, fact);
+
+    return 0;
+}
