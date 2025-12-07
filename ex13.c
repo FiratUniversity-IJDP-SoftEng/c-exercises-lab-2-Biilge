824d90844dbb0d75b4347a7da0d6027c8bfef2ac
Biilge <biilgesena@gmail.com>
2025-12-07 22:47:28 +0300

    Implement factorial function and user input

--- ex13.c
+++ ex13.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0)      // 0! = 1
+        return 1;
+
+    return n * factorial(n - 1);  
+}
+
+int main(void)
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("Negative numbers do not have factorial.\n");
+        return 0;
+    }
+
+    printf("%d! = %d\n", n, factorial(n));
+
+    return 0;
+}
 
