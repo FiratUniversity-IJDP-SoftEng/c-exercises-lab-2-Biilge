0a8b90113057b4e8c4af85df16fcffe887579242
Biilge <biilgesena@gmail.com>
2025-12-07 22:21:14 +0300

    Add a program to print formatted table of values

--- ex07.c
+++ ex07.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+    for(int i = 1; i <= 10; i++) {
+        int col1 = i * 5;
+        int col2 = 100 - (( i - 1 ) * 10 + 1);
+        int col3 = i;
+        printf("%-4d %-4d %-4d\n", col1, col2, col3);
+    }
+}
