38782c45efddb106693013c5ced32552e04f2585
Biilge <biilgesena@gmail.com>
2025-12-07 22:56:33 +0300

    Implement number filtering and printing
    Add a program to print numbers from 100 to 50 that are divisible by 3.

--- ex06.c
+++ ex06.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int numbers[100];
+    int index = 0;
+
+    for (int n = 100; n >= 50; n--)
+    {
+        if (n % 3 == 0)
+        {
+            numbers[index] = n;
+            index++;
+        }
+    }
+
+    // array elements
+    for (int i = 0; i < index; i++)
+    {
+        printf("%d ", numbers[i]);
+    }
+
+    return 0;
+}
