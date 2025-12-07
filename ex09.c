7e075c4d25823ae06100b5985b822aedc82902e0
Biilge <biilgesena@gmail.com>
2025-12-07 22:23:01 +0300

    Add number input and average calculation
    Implement a program to calculate the sum and average of user-entered numbers.

--- ex09.c
+++ ex09.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  int sum = 0;
+  int count = 0;    
+  float average;
+
+  while(1) {
+    printf("Enter number (-1 finishes): ");
+    scanf("%d", &n);
+
+    if (n == -1) {
+      break;
+    }
+
+    sum += n;
+    count++;    
+  }
+
+  if (count > 0) {
+    average = (float)sum / count;  
+    printf("\nThe numbers entered: %d\n", count);
+    printf("Addition: %d\n", sum);
+    printf("Average: %.2f\n", average);
+  } 
+  
+  else {
+    printf("\nNo numbers were entered.\n");
+  } 
+}
 
