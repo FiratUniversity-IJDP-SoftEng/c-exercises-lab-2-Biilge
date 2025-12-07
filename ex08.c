372dd63d5bbfb979d3a5a3a51dc142ebda2ef356
Biilge <biilgesena@gmail.com>
2025-12-07 22:22:17 +0300

    Implement sum and average calculation for array

--- ex08.c
+++ ex08.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d. Enter number: ", i + 1);
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    float average = sum / 10.0;  
+
+     // Output the results. 
+    printf("\nInputs:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    printf("\nAddition: %d\n", sum);
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
 
